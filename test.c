6d13b27272aa801aac2b06b53dccaf0a673db596
csss-oss <kjhje9352@naver.com>
2025-05-18 18:14:36 +0900

    Update test.c

--- test.c
+++ test.c
@@ -2,9 +2,7 @@
 
 int main(void)
 {
-	printf("bangguking good hi,hello,hi bro\n");
-	printf("cute 하시네요 형\n");
-	printf("난 짱이야\n");
+	printf("찬혁이 1번째줄 나 2번째줄 웅이 3번째줄 대훈이형 4번째줄에 작성\n"); 
 	return 0;
 }
 
